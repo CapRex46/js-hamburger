736ea46809b7f1498bfa13b6803121d17044255d
agres <ale.agresta46@gmail.com>
2021-12-09 14:30:28 +0100

    fix

---
+++ main.js
@@ -0,0 +1,5 @@
+const button = document.getElementsByClassName('fas');
+
+button.addEventListener('click', function(){
+    outputHtml.innerHTML = document.getElementsByClassName('hamburger-menu')
+})
